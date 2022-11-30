ffd187a257415f95da6875258db0604af0fa0f2c
Daniil <daniil.gudkows@gmail.com>
2022-11-30 22:35:05 +0300

    example004 if else исправление бага

--- exampl005_IfElse/Program.cs
+++ exampl005_IfElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите свое имя: ");
 string username = Console.ReadLine();
 
-if(username =="Маша")
+if(username.ToLower() =="маша")
 {
     Console.WriteLine("Ура, это же МАША!!");
 }
