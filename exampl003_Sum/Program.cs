9cd6454cf80acd2cadbf6af4b009b0e6ac4808c9
Daniil <daniil.gudkows@gmail.com>
2022-11-30 22:03:01 +0300

    sum rundom numbers

--- exampl003_Sum/Program.cs
+++ exampl003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(4, 389);
+int numberB = new Random().Next(9, 45);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
