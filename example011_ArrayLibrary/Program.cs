7481b9faf7072540a1f1955f81dcb62e9b7834ea
Daniil <daniil.gudkows@gmail.com>
2022-12-04 02:39:30 +0300

    example011 Array library

---
+++ example011_ArrayLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int n = collection.Length;
+    int index = 0;
+    while(index<n)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int Length = col.Length;
+    int position = 0;
+    while(position<Length)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
